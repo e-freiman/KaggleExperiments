d8f908a7ba76dfcedcd62f6d9d6d76dc4a10c40a
Eugene Freyman <evg.freyman@gmail.com>
2018-01-22 21:06:52 +0000

    polished description

--- MNIST-MLPack/main.cpp
+++ MNIST-MLPack/main.cpp
@@ -1,4 +1,15 @@
+/**
+ * An example of using Feed Forward Neural Network (FFN) for 
+ * solving Digit Recognizer problem from Kaggle website.
+ * 
+ * The full description of a problem as well as datasets for training 
+ * and testing are available here https://www.kaggle.com/c/digit-recognizer
+ * 
+ * @author Eugene Freyman
+ */
+
 #include <iostream>
+#include <fstream>
 
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/adam/adam.hpp>
@@ -12,23 +23,53 @@ using namespace mlpack::optimization;
 
 using namespace arma;
 
-// Returns accuracy (percentage of correct answers)
-double accuracy(const mat& predOut, const mat& realY)
+/************************* Utility functions *************************/
+
+// Saves prediction into specifically formated CSV file suitable for Kaggle
+void save(const std::string filename, const mat& pred)
 {
-  // Variable for storing correct labels
+	std::ofstream out(filename);
+	out << "ImageId,Label" << endl;
+	for (int j = 0; j < pred.n_cols; ++j)
+	{
+	  // j + 1 because Kaggle indexes start from 1
+	  // pred - 1 because 1st class is 0, 2nd class is 1 and etc.
+		out << j + 1 << "," << std::round(pred(0, j)) - 1;
+    // to avoid an empty line in the end of the file
+		if (j < pred.n_cols - 1)
+		{
+		  out << endl;
+		}
+	}
+	out.close();
+}
+
+// Returns labels bases on predicted probability of classes
+// from 1 to 10
+mat getLabels(const mat& predOut) 
+{
+  // Variable for storing predicted labels
   mat pred = arma::zeros<mat>(1, predOut.n_cols);
   
   // predOut contains 10 rows, each corresponds a class and contains
-  // the log of probability of datapont to be in that class. Class of 
-  // a datapont is choosed to be the one with maximum of log of probability.
+  // the log of probability of data point to belong to that class. Class of 
+  // a data point is chosen to be the one with maximum of log of probability.
   for (int j = 0; j < predOut.n_cols; ++j)
   {
     pred(j) = arma::as_scalar(arma::find(
         arma::max(predOut.col(j)) == predOut.col(j), 1)) + 1;
   }
   
-  // Calculating how many predicted classes are coincide with proper
-  // labels.
+  return pred;
+}
+
+// Returns accuracy (percentage of correct answers).
+double accuracy(const mat& predOut, const mat& realY)
+{
+  // Getting labels from predicted output.
+  mat pred = getLabels(predOut);
+  
+  // Calculating how many predicted classes are coincide with real labels.
   int success = 0;
   for (int j = 0; j < realY.n_cols; j++) {
     if (std::round(pred(j)) == std::round(realY(j))) {
@@ -36,70 +77,78 @@ double accuracy(const mat& predOut, const mat& realY)
     }  
   }
   
-  // Calcualting percentage of correct classifications
+  // Calculating percentage of correct classified data points.
   return (double)success / (double)realY.n_cols * 100.0;
 }
 
+/************************* Entry point *************************/
 int main()
 {
   // Dataset is randomly split into training 
-  // and test parts with followig ratio.
-  constexpr double RATIO = 0.8;
+  // and validation parts with following ratio.
+  constexpr double RATIO = 0.9;
   // The number of neurons in the first layer.
-  constexpr int H1 = 600;
+  constexpr int H1 = 28*28;
   // The number of neurons in the second layer.
-  constexpr int H2 = 600;
+  constexpr int H2 = 28*28;
   
   // The solution is done in several approaches (CYCLES), each approach 
-  // uses previous results as starting point and have different optimizer 
+  // uses previous results as starting point and have a different optimizer 
   // options (here the step size is different).
   
   // Number of iteration per cycle. 
   constexpr int ITERATIONS_PER_CYCLE = 5000;
   
   // Number of cycles.
-  constexpr int CYCLES = 50;
+  constexpr int CYCLES = 100;
   
   // Initial step size of an optimizer.
   constexpr double STEP_BEGIN = 1e-2;
   
   // Final step size of an optimizer. Between those two points step size is
   // vary linearly.
-  constexpr double STEP_END = 1e-3;
+  constexpr double STEP_END = 5e-4;
   
   std::cout << "Reading data ..." << std::endl;
   
-  // Labeled dataset that contins data for training is loaded from csv file,
+  // Labeled dataset that contains data for training is loaded from CSV file,
   // rows represent features, columns represent data points.
-  mat dataset;
-  data::Load("../../MNIST-Data/train.csv", dataset, true);
+  // Originally on Kaggle dataset CSV file has header, so it's necessary to
+  // get rid of the this row. In Armadillo representation it's the first column.
+  mat tempDataset;
+  //TODO: Here you should put proper path to train.csv file, which could
+  //be downloaded from https://www.kaggle.com/c/digit-recognizer/data
+  data::Load("../../MNIST-Data/train.csv", tempDataset, true);
+  mat dataset = tempDataset.submat(0, 1, 
+    tempDataset.n_rows - 1, tempDataset.n_cols - 1);
   
   const int N = dataset.n_cols;
   
-  // The index that splites dataset on two parts: for training and testing.
+  // The index that splits dataset on two parts: for training and validating.
   const int SPLITTER = N * 0.8;
   
   // Generating row of indexes of features (rows of dataset from 1 to 
   // dataset.n_rows - 1).
   const Row<uword> featureIdx = regspace<Row<uword>>(1, dataset.n_rows - 1);
 
-  // Generating shuffled row of indexes of datapoits.
+  // Generating shuffled row of indexes of data poits (columns of dataset from
+  // 0 to dataset.n_cols - 1).
   const Row<uword> idx = shuffle(regspace<Row<uword>>(0,  N - 1));
   
-  // Getting indexes of trining subset of data points.
+  // Getting indexes of training subset of data points.
   const Row<uword> trainIdx = idx.subvec(0, SPLITTER - 1);
-  // Getting indexes of testing subset of data points.
-  const Row<uword> testIdx = idx.subvec(SPLITTER, idx.n_elem - 1);
+  // Getting indexes of validating subset of data points.
+  const Row<uword> validIdx = idx.subvec(SPLITTER, idx.n_elem - 1);
   
   // Getting training dataset with features (subset of original dataset
-  // with selectes for training indexes)
+  // with selected indexes for training).
   const mat trainX = dataset.submat(featureIdx, trainIdx);
-  // Getting testing dataset with features (subset of original dataset
-  // with selectes for testing indexes).
-  const mat testX = dataset.submat(featureIdx, testIdx);
+  // Getting validating dataset with features (subset of original dataset
+  // with selected indexes for validating).
+  const mat validX = dataset.submat(featureIdx, validIdx);
 
   // According to NegativeLogLikelihood output layer of NN, labels should
-  // specify class of a datapoint and be in the interval from 1 to 
+  // specify class of a data point and be in the interval from 1 to 
   // number of classes (in this case from 1 to 10).
   
   // Creating labels for training.
@@ -109,17 +158,17 @@ int main()
     trainY(0, j) = dataset(0, trainIdx(j)) + 1;
   }
   
-  // Creating labels for testing.
-  mat testY(1, tempTestY.n_cols);
-  for (int j = 0; j < testIdx.n_cols; ++j)
+  // Creating labels for validating.
+  mat validY(1, validIdx.n_cols);
+  for (int j = 0; j < validIdx.n_cols; ++j)
   {
-    testY(0, j) = dataset(0, testIdx(j)) + 1;
+    validY(0, j) = dataset(0, validIdx(j)) + 1;
   }
   
-  // Specifing the NN model. NegativeLogLikelihood is the output layer that
+  // Specifying the NN model. NegativeLogLikelihood is the output layer that
   // is used for classification problem. RandomInitialization means that 
-  // initial weights in neurons are going to be generated randomly
-  // in the interval from -1 to 1.
+  // initial weights in neurons are generated randomly in the interval 
+  // from -1 to 1.
   FFN<NegativeLogLikelihood<>, RandomInitialization> model;
   // This is intermediate layer that is needed for connection between input
   // data and sigmoid layer. Parameters specify the number of input features
@@ -131,7 +180,7 @@ int main()
   model.Add<Linear<> >(H1, H2);
   // The second sigmoid layer.
   model.Add<SigmoidLayer<> >();
-  // Dropout layer for regularization. First parameter - the probability of
+  // Dropout layer for regularization. First parameter is the probability of
   // setting a specific value to 0.
   model.Add<Dropout<> >(0.3, true);
   // Intermediate layer.
@@ -142,11 +191,11 @@ int main()
   
   std::cout << "Training ..." << std::endl;  
   
-  // Cycles for minitoring a solution and applaying different step size of 
+  // Cycles for monitoring a solution and applying different step size of 
   // the Adam optimizer.
   for (int i = 0; i <= CYCLES; i++) {
     
-    // Calculating a step size as linearly distributed between specifed 
+    // Calculating a step size as linearly distributed between specified 
     // STEP_BEGIN and STEP_END values.
     double stepRatio = (double)i / (double)CYCLES;
     double step = STEP_BEGIN + stepRatio * (STEP_END - STEP_BEGIN);
@@ -159,19 +208,42 @@ int main()
     model.Train(trainX, trainY, opt);  
     
     mat predOut;
-    // Getting predictions on training dataset.
+    // Getting predictions on training data points.
     model.Predict(trainX, predOut);
-    // Calculating acccuracy on training dataset.
+    // Calculating accuracy on training data points.
     double trainAccuracy = accuracy(predOut, trainY);
-    // Getting predictions on test dataset.
-    model.Predict(testX, predOut);
-    // Calculating acccuracy on test dataset.
-    double testAccuracy = accuracy(predOut, testY);
+    // Getting predictions on validating data points.
+    model.Predict(validX, predOut);
+    // Calculating accuracy on validating data points.
+    double validAccuracy = accuracy(predOut, validY);
 
     std::cout << i << ", step = " << step << ", accuracy"
       " train = "<< trainAccuracy << "%," << 
-      " test = "<< testAccuracy << "%" <<  std::endl;
+      " valid = "<< validAccuracy << "%" <<  std::endl;
   }
   
+  std::cout << "Predicting ..." << std::endl;  
+  
+  // Loading test dataset (the one whose predicted labels 
+  // should be sent to Kaggle website).
+  // As before, it's necessary to get rid of header
+  
+  //TODO: Here you should put proper path to test.csv file, which could
+  //be downloaded from https://www.kaggle.com/c/digit-recognizer/data
+  data::Load("../../MNIST-Data/test.csv", tempDataset, true);
+  mat testX = tempDataset.submat(0, 1, 
+    tempDataset.n_rows - 1, tempDataset.n_cols - 1);
+
+  mat testPredOut;
+  // Getting predictions on test data points 
+  model.Predict(testX, testPredOut);
+  // Generating labels for test dataset
+  mat testPred = getLabels(testPredOut);
+  cout << "Saving predicted labels to \"results.csv\" ..." << endl;
+  
+  // TODO: result.csv could be uploaded to 
+  // https://www.kaggle.com/c/digit-recognizer/submissions for competition
+  save("results.csv", testPred);
+ 
   std::cout << "Finished" << std::endl;
 }
